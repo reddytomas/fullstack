4437ee0ff1e903c112ca663ce1bfc2eef1e1ec84
Nicolas Nuñez <nicolaasnunez@gmail.com>
2019-09-11 01:34:00 -0300

    carritodecompras casi terminada

---
+++ php/header.php
@@ -0,0 +1,28 @@
+<header>
+  <!-- navbar -------------------------------------->
+  <nav class="navbar navbar-expand-lg navbar-light _ni_navbar mb-5">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav ">
+      <a class="nav-item nav-link active" href="index.html">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="productos.html">Productos</a>
+      <a class="nav-item nav-link" href="faq.html">FAQ</a>
+      <a class="nav-item nav-link" href="contacto.html">Contacto</a>
+      <a class="nav-item nav-link" href="registrate.html">Registrate</a>
+      <a class="nav-item nav-link" href="registrate.html">Iniciar sesion</a>
+    </div>
+
+    </div>
+
+    <!-- iniciar, registro o mi perfil ----------------------->
+    <ul class="_perfilList">
+      <a class="nav-item nav-link"href="perfil.html" id="iniciar"><li><i class="fas fa-user-circle fa-1x"></i></li></a>
+      <a class="btn btn-primary btn-lg" href="carritodecompras.html" role="button"><li><i class="fas fa-shopping-cart"></i></li></a>
+
+    </ul>
+  </nav>
+
+</header>
