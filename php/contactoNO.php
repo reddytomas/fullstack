bc2c91bc8c8c5521327e98961cfb82988f8b7ead
reddytomas <reddy.tomas@gmail.com>
2019-09-25 12:49:36 -0300

    paginas php

---
+++ php/contactoNO.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <!-- Bootstrap CSS -->
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Fuentes y nuestro stylesheet -->
+    <link href="https://fonts.googleapis.com/css?family=Ceviche+One|Sedgwick+Ave|Sedgwick+Ave+Display&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Playfair+Display|Raleway|Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css">
+
+    <link rel="stylesheet" href="css/master.css">
+    <title>Carrito</title>
+  </head>
+
+<body>
+  <div class="container-fluid m-0 p-0">
+    <header>
+      <!-- navbar -------------------------------------->
+      <nav class="navbar navbar-expand-lg navbar-light _ni_navbar mb-5">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav d-flex justify-content-around">
+          <a href="index.html"  class="text-dark mr-3" id="_altasllantasfooter">Altas Llantas</a>
+          <a class="nav-item nav-link " href="index.html">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="productos.html">Productos</a>
+          <a class="nav-item nav-link" href="faq.html">FAQ</a>
+          <a class="nav-item nav-link" href="contacto.html">Contacto</a>
+          <a class="nav-item nav-link" href="registrate.html">Registrate</a>
+          <a class="nav-item nav-link" href="login.html">Iniciar sesion</a>
+        </div>
+
+        </div>
+
+        <!-- iniciar, registro o mi perfil ----------------------->
+        <ul class="_perfilList">
+          <a class="nav-item nav-link"href="perfil.html" id="iniciar"><li><i class="fas fa-user-circle fa-1x"></i></li></a>
+          <a class="btn btn-primary btn-lg" href="carritodecompras.html" role="button"><li><i class="fas fa-shopping-cart"></i></li></a>
+
+        </ul>
+      </nav>
+
+    </header>
+
+
+
+    <!-- CONTACTO  y maps----------------------------->
+    <section class="_section1contacto">
+
+    <img src="IMG/logo.jpg" alt="" height="80px;" class="_logoencontacto">
+    </section>
+    <section class="_contacto row p-3">
+      <article class="col-sm-12 col-md-6">
+        <form>
+          <h6>Contactanos</h6>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore temporibus aut distinctio eligendi molestiae, optio recusandae, minus ut eum beatae architecto consequatur doloribus aliquam enim dolore quis officiis, sapiente dolores maxime in alias eveniet et voluptatem nobis? Voluptas laudantium porro explicabo adipisci officiis placeat, delectus obcaecati ducimus consectetur ipsum. A?</p>
+          <p>email:<br>   altasllantas@gmail.com</p>
+          <p>telefono: <br>   +54 9 11 1234 5678</p>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Email</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+          </div>
+
+          <div class="form-group">
+            <label for="exampleFormControlTextarea1">Comentario</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+          <button type="button" class="btn btn-primary">Enviar</button>
+        </form>
+
+    </section>
+    <!-- FOOTER con redes y nada mas -->
+
+    <footer class="bg-light w-100">
+      <p class="text-center" id="_altasllantasfooter">Altas Llantas</p>
+      <ul class="list-unstyled list-group-horizontal d-flex justify-content-around ">
+        <li><a href=""><i class="fab fa-instagram fa-2x "></i></a></li>
+        <li><a href=""><i class="fab fa-facebook-square fa-2x"></i></a></li>
+        <li><a href=""><i class="fab fa-twitter-square fa-2x"></i></a></li>
+      </ul>
+
+    </footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </div>
+  </body>
+</html>
---
+++ php/faq.php
@@ -0,0 +1,146 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <!-- Bootstrap CSS -->
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Fuentes y nuestro stylesheet -->
+    <link href="https://fonts.googleapis.com/css?family=Ceviche+One|Sedgwick+Ave|Sedgwick+Ave+Display&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Playfair+Display|Raleway|Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css">
+
+    <link rel="stylesheet" href="css/faq.css">
+    <title>Carrito</title>
+  </head>
+
+<body>
+  <div class="container-fluid m-0 p-0">
+    <header>
+      <!-- navbar -------------------------------------->
+      <nav class="navbar navbar-expand-lg navbar-light _ni_navbar mb-5">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav d-flex justify-content-around">
+          <a href="index.html"  class="text-dark mr-3" id="_altasllantasfooter">Altas Llantas</a>
+          <a class="nav-item nav-link " href="index.html">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="index.html#productos">Productos</a>
+          <a class="nav-item nav-link" href="faq.html">FAQ</a>
+          <a class="nav-item nav-link" href="index.html#contacto">Contacto</a>
+          <a class="nav-item nav-link" href="registrate.html">Registrate</a>
+          <a class="nav-item nav-link" href="login.html">Iniciar sesion</a>
+        </div>
+
+        </div>
+
+        <!-- iniciar, registro o mi perfil ----------------------->
+        <ul class="_perfilList">
+          <a class="nav-item nav-link"href="perfil.html" id="iniciar"><li><i class="fas fa-user-circle fa-2x"></i></li></a>
+          <a class="btn btn-primary btn-lg" href="carritodecompras.html" role="button"><li><i class="fas fa-shopping-cart"></i></li></a>
+
+        </ul>
+      </nav>
+
+    </header>
+      <!-- preguntas frecuentes -------------------------------------->
+        <h1 class="_tituloPagina ml-1 mr-1  bg-light text-dark pl-3 mb-3  text-center">Preguntas Frecuentes</h1><br><br>
+<section  class="_sectionfaq">
+      <main>
+
+        <div>
+    <label for="item-1">Pregunta 1:</label>
+    <input type="checkbox" name="one" id="item-1" checked>
+    <div class="hide">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labor
+      </p>
+    </div>
+
+     <label for="item-2">Pregunta 2:</label>
+    <input type="checkbox" name="two" id="item-2" checked>
+    <div class="hide">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labor</p>
+    </div>
+
+     <label for="item-3">Pregunta 3:</label>
+    <input type="checkbox" name="three" id="item-3" checked>
+    <div class="hide">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labor</p>
+    </div>
+
+    <label for="item-4">Pregunta 4:</label>
+   <input type="checkbox" name="four" id="item-4" checked>
+   <div class="hide">
+   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut laborv</p>
+   </div>
+
+   <label for="item-5">Pregunta 5:</label>
+  <input type="checkbox" name="five" id="item-5" checked>
+  <div class="hide">
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labor</p>
+  </div>
+
+  <label for="item-6">Pregunta 6:</label>
+ <input type="checkbox" name="six" id="item-6" checked>
+ <div class="hide">
+ <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labor</p>
+ </div>
+
+ <label for="item-7">Pregunta 7:</label>
+<input type="checkbox" name="seven" id="item-7" checked>
+<div class="hide">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labor</p>
+</div>
+
+<label for="item-8">Pregunta 8:</label>
+<input type="checkbox" name="eight" id="item-8" checked>
+<div class="hide">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labor</p>
+</div>
+
+<label for="item-9">Pregunta 9:</label>
+<input type="checkbox" name="nine" id="item-9" checked>
+<div class="hide">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labor</p>
+</div>
+
+<label for="item-10">Pregunta 10:</label>
+<input type="checkbox" name="ten" id="item-10" checked>
+<div class="hide">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labor</p>
+</div>
+
+      </main>
+</section>
+
+<!-- PRODUCTOS ---------------------------------------------->
+
+
+    <!-- CONTACTO  y maps----------------------------->
+
+    <!-- FOOTER con redes y nada mas -->
+    <footer class="bg-light w-100">
+      <p class="text-center" id="_altasllantasfooter">Altas Llantas</p>
+      <ul class="list-unstyled list-group-horizontal d-flex justify-content-around ">
+        <li><a href=""><i class="fab fa-instagram fa-2x "></i></a></li>
+        <li><a href=""><i class="fab fa-facebook-square fa-2x"></i></a></li>
+        <li><a href=""><i class="fab fa-twitter-square fa-2x"></i></a></li>
+      </ul>
+      <div class="text-center text-dark"><a href="#" class="">Volver arriba</a></div>
+
+    </footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </div>
+  </body>
+</html>
