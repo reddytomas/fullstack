1d3a0a5073975f4fe355a0f9c2d04fbd64520a51
tomas grimaux <tgrimaux@gmail.com>
2019-10-08 20:51:25 -0400

    tomi, va la ultima clase, fijate que es el ejercico que sigue a la explicacion de static, herencia, interfase, etc

---
+++ practica php 4 - 07-10-2019/clases/cuenta.php
@@ -0,0 +1,46 @@
+<?php
+require_once("cliente.php");
+
+abstract class cuenta extends Cliente{
+  private $cbu;
+  private $balance = 0;
+  private $fechaUltimoM = 0;
+
+
+public function __construct(int $cbu) {
+  $this->cbu = $cbu;
+
+}
+
+public function setCbu($cbu){
+  $this->cbu = $cbu;
+}
+public function getCbu(){
+  return $this->cbu;
+  }
+public function setBalance($balance){
+  $this->balance = $balance;
+}
+public function getBalance(){
+  return $this->balance;
+}
+public function getFechaUltimoM($fechaUltimoM){
+  $this->fechaUltimoM = $fechaUltimoM;
+}
+public function getFechaUltimoM(){
+  return $this->fechaUltimoM;
+}
+
+abstract public function debitar($monto, $origen);
+
+public function acreditar($monto){
+  $this->balance += $monto;
+  $this->actualizarFechaTran();
+}
+protected function actualizarFechaTran(){
+  $this->fechaUltimoM = displaDate ('YYYY-MM-JJ');
+}
+
+}
+
+ ?>
